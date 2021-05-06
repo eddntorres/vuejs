ff5ab0cc650f530cc91d025cf1e2300d78ca6812
Eduardo Torres <eduk_1012@hotmail.com>
2021-05-06 00:46:34 -0500

    tarea 2 finalizada

--- 5.-basics-assignment-2-problem/app.js
+++ 5.-basics-assignment-2-problem/app.js
@@ -1,13 +1,20 @@
 const app= Vue.createApp({
     data(){
         return {
-
+            inputValue: '',
+            inputValue2:''
         };
     },
     methods: {
         clickAlert(){
             alert('Alerta');
-        }
+        },
+        inputChange(event){
+            this.inputValue = event.target.value;
+        },
+        inputChange2(event){
+            this.inputValue2 = event.target.value;
+        }        
     }
 });
 
