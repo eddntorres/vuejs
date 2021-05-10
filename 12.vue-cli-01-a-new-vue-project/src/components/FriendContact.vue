8cd792c36a9e1db41b6d4ec5b624f56ab5bf1b16
Eduardo Torres <eduk_1012@hotmail.com>
2021-05-10 10:36:15 -0500

    Vue js Components

---
+++ 12.vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,34 @@
+<template>
+    <li>
+        <h2>{{friend.name}}</h2>
+        <button @click="toggleDetails">{{detailsAreVisible ? 'Hide Details': 'Show Details'}}</button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone: </strong> {{friend.phone}}</li>
+            <li><strong>Email: </strong>{{friend.email}}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+   data(){
+       return{
+           detailsAreVisible: false,
+           friend:
+                {
+                    id: 'julie',
+                    name: 'Julie Jones',
+                    phone: '0123912 12312',
+                    email: 'julie@localhost.com'
+
+                }                 
+           
+       };
+   },
+   methods:{
+       toggleDetails(){
+           this.detailsAreVisible = !this.detailsAreVisible;
+       }
+   }
+};
+</script>
