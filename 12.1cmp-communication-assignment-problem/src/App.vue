e0782f33d6943adf13e3766312538f4bb9f8632c
Eduardo Torres <eduk_1012@hotmail.com>
2021-05-13 02:04:46 -0500

    excercise resolve

---
+++ 12.1cmp-communication-assignment-problem/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <user-data @update-data="userUpdateData">
+    </user-data>
+    <active-user :name="name" :age="age">
+    </active-user>
+</template>
+
+<script>
+import UserData from './components/UserData.vue'
+export default {
+  components: { UserData },
+    data(){
+        return{
+            name: 'Eduardo',
+            age: 24
+        }
+    },
+    methods: {
+        userUpdateData(userName, userAge){
+            this.name = userName;
+            this.age = userAge
+        }
+    }
+}
+
+</script>
+
---
+++ 12.1cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,24 @@
+<template>
+    <form @submit.prevent="updateData">    
+        <input v-model="newName" type="text" placeholder="name">
+        <input v-model="newAge" type="text" placeholder="age">
+        <button type="submit">ENVIAR</button>  
+    </form>
+</template>
+
+<script>
+export default {
+    emits:['update-data'],
+    data(){
+        return{
+            newName: '',
+            newAge: 0
+        }
+    },
+    methods:{
+        updateData(){
+            this.$emit('update-data',this.newName, this.newAge);
+        }
+    }
+}
+</script>
